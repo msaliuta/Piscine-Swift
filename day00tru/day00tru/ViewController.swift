71c43d5ff047f8735cce96a1e63e939b4d086427
msaliuta <msaliuta@student.unit.ua>
2020-02-03 23:23:32 +0200

    fix was added

--- day00tru/day00tru/ViewController.swift
+++ day00tru/day00tru/ViewController.swift
@@ -15,51 +15,23 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBAction func test(_ sender: UIButton) {
+        let nbr:Int? = Int(result_LBL.text!)
+        
+        if (nbr != 0){
+            result_LBL.text! += String(sender.tag)
+        }
+        else {
+            result_LBL.text! = String(sender.tag)
+        }
+    }
+    
+    
+    @IBAction func clean(_ sender: UIButton) {
+        result_LBL.text = "0";
+    }
+    
     @IBOutlet weak var result_LBL: UILabel!
-    
-    
-    @IBAction func ac_BTN(_ sender: UIButton) {
-        result_LBL.text = "0";
-    }
-    
-    @IBAction func first_BTN(_ sender: UIButton) {
-        result_LBL.text = "1";
-    }
-    
-    @IBAction func second_BTN(_ sender: UIButton) {
-        result_LBL.text = "2";
-    }
-    
-    @IBAction func third_BTN(_ sender: UIButton) {
-        result_LBL.text = "3";
-    }
 
-    @IBAction func fourth_BTN(_ sender: UIButton) {
-        result_LBL.text = "4";
-    }
-    
-    @IBAction func fifth_BTN(_ sender: UIButton) {
-        result_LBL.text = "5";
-    }
-    
-    @IBAction func sixth_BTN(_ sender: UIButton) {
-        result_LBL.text = "6";
-    }
-    
-    @IBAction func seventh_BTN(_ sender: UIButton) {
-        result_LBL.text = "7";
-    }
-    
-    @IBAction func eights_BTN(_ sender: UIButton) {
-        result_LBL.text = "8";
-    }
-    
-    @IBAction func nineth_BTN(_ sender: UIButton) {
-        result_LBL.text = "0";
-    }
-    
-    @IBAction func zero_BTN(_ sender: UIButton) {
-        result_LBL.text = "0";
-    }
 }
 
