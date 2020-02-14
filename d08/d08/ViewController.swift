337ac67cb149fd8d838f0406960360b708408636
msaliuta <msaliuta@student.unit.ua>
2020-02-14 23:41:23 +0200

    day09

---
+++ d08/d08/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  d08
+//
+//  Created by Maksym SALIUTA on 2/14/20.
+//  Copyright © 2020 Maksym SALIUTA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
